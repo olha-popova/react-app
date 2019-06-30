d314e229e83a988f232b98092d9af525e7f16641
Olha Popova <popovaolgad@gmail.com>
2019-06-30 14:12:49 +0300

    Initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <img src="https://wallpaperplay.com/walls/full/e/8/c/282712.jpg" alt=""/>
+            <div>
+                Avatar + description
+            </div>
+            <div>
+                My posts
+
+                <div>
+                    New post
+                </div>
+
+                <div>
+                    <div>My post 1</div>
+                    <div>My post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
